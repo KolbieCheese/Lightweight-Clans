b2436e3e01790408eec738984141a15eb4560142
Bib <masterchiefkbr@gmail.com>
2026-03-30 13:23:55 -0400

    Map exclamation mark to i in moderation normalization

--- src/main/java/io/github/maste/customclans/util/ValidationUtil.java
+++ src/main/java/io/github/maste/customclans/util/ValidationUtil.java
@@ -118,7 +118,7 @@ public final class ValidationUtil {
             case '4', '@' -> 'a';
             case '5', '$' -> 's';
             case '7' -> 't';
-            case '*' -> 'u';
+            case '!', '*' -> 'i';
             default -> character;
         };
     }
